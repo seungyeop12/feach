b14807331387dd9f0885bc5af9c95bc36cb15350
seungyeop12 <art7449@gmail.com>
2023-09-22 10:47:39 +0900

    이미지 블러처리

--- main.js
+++ main.js
@@ -1,10 +1,24 @@
+const section = document.querySelector('section');
+
 fetch('DB/department.json')
 	.then((data) => {
-		console.log(data);
-		const result = data.json();
-		result.then((json) => {
-			console.log(json);
+		return data.json();
+	})
+	.then((json) => {
+		let tags = '';
+		json.members.map((data) => {
+			tags += `
+        <article>
+          <div class='pic'>
+            <img src='img/${data.pic}'>
+            <img src='img/${data.pic}'>
+          </div>
+          <h2>${data.name}</h2>
+          <p>${data.position}</p>
+        </article>
+      `;
 		});
+		section.innerHTML = tags;
 	})
 	.catch((err) => {
 		console.log(err);
